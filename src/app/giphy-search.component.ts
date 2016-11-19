623b24a0211424e451348f854d4605766a2465b9
pkasprzyk <pkasprzyk@future-processing.com>
2016-11-19 21:24:42 +0100

    all changes

--- src/app/giphy-search.component.ts
+++ src/app/giphy-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
   moduleId: module.id,
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class GiphySearchAppComponent {
   title = 'giphy-search works!';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  giphies = [];
+  
+  constructor(http: Http) { this.http = http; }
+  
+  performSearch(searchTerm: HTMLInputElement): void {
+    var apiLink = this.link + searchTerm.value;
+    this.http.request(apiLink).subscribe((res: Response) => { 
+      this.giphies = res.json().data;
+      console.log(this.giphies);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { GiphySearchAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(GiphySearchAppComponent);
+bootstrap(GiphySearchAppComponent, [HTTP_PROVIDERS]);
